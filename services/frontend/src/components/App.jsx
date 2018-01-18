bea81625cae154d616ed6dba24042457e55c9d62
AleF83 <AlexanderFux83@gmail.com>
2018-01-18 13:39:22 +0200

    Simple react-router added to frontend

--- services/frontend/src/components/App.jsx
+++ services/frontend/src/components/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
-const App = () =>
-  <div>Welcome to RPGM Frontend!</div>;
+const App = () => (
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Welcome to RPGM Frontend!</h1>} />
+      <Route exact path="/login" render={() => <h2>You need to login</h2>} />
+    </Switch>
+  </HashRouter>
+);
 
 export default App;
