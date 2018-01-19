13739178e9bc8d0047310c0a0f5554199b09c143
AleF83 <AlexanderFux83@gmail.com>
2018-01-19 16:34:47 +0200

    Redux added to Frontend

--- services/frontend/src/components/App.jsx
+++ services/frontend/src/components/App.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import {
+	ConnectedRouter,
+	routerReducer,
+	routerMiddleware
+} from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
+export const store = createStore(
+	combineReducers({ router: routerReducer }),
+	{},
+	applyMiddleware(routerMiddleware(history))
+);
 
 const App = () => (
-  <HashRouter>
-    <Switch>
-      <Route exact path="/" render={() => <h1>Welcome to RPGM Frontend!</h1>} />
-      <Route exact path="/login" render={() => <h2>You need to login</h2>} />
-    </Switch>
-  </HashRouter>
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<Switch>
+				<Route
+					exact
+					path="/"
+					render={() => <h1>Welcome to RPGM Frontend!</h1>}
+				/>
+				<Route
+					exact
+					path="/login"
+					render={() => <h2>You need to login</h2>}
+				/>
+			</Switch>
+		</ConnectedRouter>
+	</Provider>
 );
 
 export default App;
