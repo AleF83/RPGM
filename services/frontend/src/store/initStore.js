da62085a88a7332f8aa4271492180ab452f61d58
AleF83 <AlexanderFux83@gmail.com>
2018-01-21 09:18:54 +0200

    react-router-redux added to Frontend

--- services/frontend/src/store/initStore.js
+++ services/frontend/src/store/initStore.js
@@ -1,5 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import browserHistory from './initHistory';
 
-const initStore = () => createStore(state => state, {});
+const initStore = () =>
+	createStore(
+		combineReducers({
+			router: routerReducer
+		}),
+		{},
+		applyMiddleware(routerMiddleware(browserHistory))
+	);
 
 export default initStore;
