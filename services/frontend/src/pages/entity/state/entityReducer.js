697c68b1943068c1fc8999d9b71cb7afd763a3df
Alex <AlexanderFux83@gmail.com>
2018-01-28 08:14:51 +0200

    Redux was integrated to entity page (#24)

---
+++ services/frontend/src/pages/entity/state/entityReducer.js
@@ -0,0 +1,30 @@
+import {
+  ENTITY_PROPERTY_ADDED,
+  ENTITY_PROPERTY_REMOVED,
+  ENTITY_PROPERTY_CHANGED,
+} from './entityActionTypes';
+
+const initialState = {};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ENTITY_PROPERTY_ADDED:
+      return {
+        ...state,
+        [action.propName]: action.propValue,
+      };
+    case ENTITY_PROPERTY_REMOVED:
+      return Object.keys(state).reduce(
+        (newState, key) =>
+          (key !== action.propName ? { ...newState, [key]: state[key] } : newState),
+        {},
+      );
+    case ENTITY_PROPERTY_CHANGED:
+      return {
+        ...state,
+        [action.propName]: action.propValue,
+      };
+    default:
+      return state;
+  }
+};
