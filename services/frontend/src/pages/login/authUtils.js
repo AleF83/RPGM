9c87e0a5e62ed75d64415839711b52e89cdcda38
AleF83 <AlexanderFux83@gmail.com>
2018-03-08 13:15:28 +0200

    Small improvenment

--- services/frontend/src/pages/login/authUtils.js
+++ services/frontend/src/pages/login/authUtils.js
@@ -1,20 +1,22 @@
 /* global localStorage window */
 import Oidc from 'oidc-client';
 
-const createOidcClientConfiguration = (id, authority, clientId) => ({
+const createOidcClientConfiguration = (id, authority, clientId, scope) => ({
   authority,
   client_id: clientId,
   redirect_uri: `${window.location.origin}/auth/${id}/`,
   silent_redirect_uri: `${window.location.origin}/auth/${id}/`,
   post_logout_redirect_uri: `${window.location.origin}/login`,
   response_type: 'id_token token',
-  scope: 'openid email profile',
+  scope,
   filterProtocolClaims: true,
   loadUserInfo: true,
+  automaticSilentRenew: true,
+  userStore: new Oidc.WebStorageStateStore({ store: localStorage }),
 });
 
-export const signinRedirect = (id, issuer, clientId, state = {}) => {
-  const oidcSettings = createOidcClientConfiguration(id, issuer, clientId);
+export const signinRedirect = (id, issuer, clientId, scope, state = {}) => {
+  const oidcSettings = createOidcClientConfiguration(id, issuer, clientId, scope);
   const oidcClient = new Oidc.UserManager(oidcSettings);
   return oidcClient.signinRedirect(state);
 };
